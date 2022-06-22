536a1588c6f3fd700ea123ef9462c4535621e4cd
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-06-22 00:21:37 -0400

    - Fix evolutions typo

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,4 +1,5 @@
-# -- Ups
+# -- !Ups
+
 create table user_agreements (
     user_id uuid not null constraint user_agreements primary key,
     terms_and_conditions_accepted varchar,
@@ -8,4 +9,4 @@ create table user_agreements (
     constraint fk_user foreign key (user_id) references users(id) on delete cascade
 );
 
-# -- Downs
+# -- !Downs
