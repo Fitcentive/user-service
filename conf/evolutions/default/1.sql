5e9c1a5089ea5c94f59a8951b8e2e5fb0caa07e7
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-06-22 00:46:58 -0400

    - Fix evolutions script type typo

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -57,8 +57,8 @@ create table username_lock (
 
 create table user_agreements (
     user_id uuid not null constraint pk_user_agreements primary key,
-    terms_and_conditions_accepted varchar,
-    subscribe_to_emails varchar,
+    terms_and_conditions_accepted boolean not null,
+    subscribe_to_emails boolean not null,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
     constraint fk_user foreign key (user_id) references users(id) on delete cascade
