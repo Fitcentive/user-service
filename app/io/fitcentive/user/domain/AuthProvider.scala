dd522f482110c7526a0c2ab941b511f3777f0c6e
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-08-22 23:26:28 -0400

    - Add Facebook auth provider

--- app/io/fitcentive/user/domain/AuthProvider.scala
+++ app/io/fitcentive/user/domain/AuthProvider.scala
@@ -9,17 +9,19 @@ trait AuthProvider {
 object AuthProvider {
   def apply(status: String): AuthProvider =
     status match {
-      case NativeAuth.stringValue => NativeAuth
-      case GoogleAuth.stringValue => GoogleAuth
-      case AppleAuth.stringValue  => AppleAuth
-      case _                      => throw new Exception("Unexpected auth provider")
+      case NativeAuth.stringValue   => NativeAuth
+      case GoogleAuth.stringValue   => GoogleAuth
+      case AppleAuth.stringValue    => AppleAuth
+      case FacebookAuth.stringValue => FacebookAuth
+      case _                        => throw new Exception("Unexpected auth provider")
     }
 
   implicit lazy val writes: Writes[AuthProvider] = {
     {
-      case NativeAuth => JsString(NativeAuth.stringValue)
-      case GoogleAuth => JsString(GoogleAuth.stringValue)
-      case AppleAuth  => JsString(AppleAuth.stringValue)
+      case NativeAuth   => JsString(NativeAuth.stringValue)
+      case GoogleAuth   => JsString(GoogleAuth.stringValue)
+      case AppleAuth    => JsString(AppleAuth.stringValue)
+      case FacebookAuth => JsString(FacebookAuth.stringValue)
     }
   }
 
@@ -35,4 +37,7 @@ object AuthProvider {
     val stringValue: String = "AppleAuth"
   }
 
+  case object FacebookAuth extends AuthProvider {
+    val stringValue: String = "FacebookAuth"
+  }
 }
---
+++ conf/evolutions/default/7.sql
@@ -0,0 +1,5 @@
+# -- !Ups
+
+insert into auth_provider_types (name, description) values ('FacebookAuth',  'Authentication using Facebook SSO');
+
+# -- !Downs
