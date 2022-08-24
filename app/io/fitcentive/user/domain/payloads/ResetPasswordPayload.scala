21d00fc29569a3b11a6c4b2f147669e2dc2f54c4
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-08-23 22:30:10 -0400

    - Remove todos

--- app/io/fitcentive/user/domain/payloads/ResetPasswordPayload.scala
+++ app/io/fitcentive/user/domain/payloads/ResetPasswordPayload.scala
@@ -2,7 +2,6 @@ package io.fitcentive.user.domain.payloads
 
 import play.api.libs.json.{Json, Reads, Writes}
 
-// todo - receive password as base64
 case class ResetPasswordPayload(email: String, newPassword: String, emailVerificationToken: String)
 
 object ResetPasswordPayload {
