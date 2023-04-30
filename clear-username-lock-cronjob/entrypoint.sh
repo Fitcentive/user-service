043593b7f1191b1e1bfe2b9df123c2bcbfa4d12a
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-04-30 12:00:02 -0400

    feat: Fix dockerfile project name for pubsub cronjob

--- clear-username-lock-cronjob/entrypoint.sh
+++ clear-username-lock-cronjob/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 gcloud auth activate-service-account --key-file=/opt/service-account/key.json
-gcloud pubsub topics publish --project=fitcentive-1210 clear-username-lock-table --message="{\"topic\":\"clear-username-lock-table\",\"payload\":{\"message\":\"Clear username lock table now\"},\"id\":\"df3bd32e-05e1-4d33-843b-44261a828222\"}"
+gcloud pubsub topics publish --project=fitcentive-dev-02 clear-username-lock-table --message="{\"topic\":\"clear-username-lock-table\",\"payload\":{\"message\":\"Clear username lock table now\"},\"id\":\"df3bd32e-05e1-4d33-843b-44261a828222\"}"
 
 exec "$@"
