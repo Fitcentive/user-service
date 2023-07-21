d7d63ff100e059ac6ba8c98536cb760ff51e1a86
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-07-20 23:51:10 -0400

    fix: Update deploy script

--- deploy_scripts/dev_deploy_to_gke.sh
+++ deploy_scripts/dev_deploy_to_gke.sh
@@ -9,3 +9,7 @@ echo "y" | gcloud container images delete gcr.io/fitcentive-dev-03/user:1.0 --fo
 sbt docker:publish
 
 kubectl apply -f deployment/gke-dev-env/
+
+cd clear-username-lock-cronjob && \
+  docker build -t gcr.io/fitcentive-dev-02/gcloud-user-cron-pubsub-image:latest -t gcr.io/fitcentive-dev-02/gcloud-user-cron-pubsub-image:1.0 . && \
+  docker push gcr.io/fitcentive-dev-03/gcloud-user-cron-pubsub-image:1.0
