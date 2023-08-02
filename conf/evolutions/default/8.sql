1180f99785da76f4e84e5c9d0bdb3e9a0adc64e0
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-08-02 16:11:56 -0400

    feat: Add events to track Progress module

---
+++ conf/evolutions/default/8.sql
@@ -0,0 +1,11 @@
+# -- !Ups
+
+insert into user_event_types (name, description) values ('ViewDetailedWeightAchievements',   'When a user views their detailed weight achievements');
+
+insert into user_event_types (name, description) values ('ViewProgress',                     'When a user views their progress home view');
+insert into user_event_types (name, description) values ('ViewDetailedStepProgress',         'When a user views their detailed step progress');
+insert into user_event_types (name, description) values ('ViewDetailedDiaryProgress',        'When a user views their detailed diary progress');
+insert into user_event_types (name, description) values ('ViewDetailedActivityProgress',     'When a user views their detailed activity progress');
+insert into user_event_types (name, description) values ('ViewDetailedWeightProgress',       'When a user views their detailed weight progress');
+
+# -- !Downs
